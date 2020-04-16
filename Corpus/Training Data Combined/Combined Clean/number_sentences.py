513cbe1be589975fb10e2bf60e167ac20c1f69f7
destinyson7 <tanish.lad@research.iiit.ac.in>
2020-04-17 03:22:06 +0530

    added code to number the sentences

---
+++ Corpus/Training Data Combined/Combined Clean/number_sentences.py
@@ -0,0 +1,26 @@
+import re
+
+sentence_number = 0
+
+sentences = []
+
+with open("/home/laaaad/IIIT-H/Courses/Semester4/Intoduction to NLP/Project/Dependency-Parser-Marathi/Corpus/Training Data Combined/Combined Clean/cleaned.txt", "r") as f:
+    for line in f:
+        stripped_line = re.sub('\s+', ' ', line)
+
+        if stripped_line:
+            if stripped_line.split(" ")[0] == "<Sentence":
+                sentence_number += 1
+
+                sentences.append("<Sentence id='" +
+                                 str(sentence_number) + "'>\n")
+
+            else:
+                sentences.append(line)
+
+        else:
+            sentences.append(line)
+
+new_file = open("numbered.txt", "w")
+new_file.writelines(sentences)
+new_file.close()
