ad4a8b3b59c28db527b2f985aff4a8f95232cfa4
destinyson7 <tanish.lad@research.iiit.ac.in>
2020-05-03 17:39:34 +0530

    model 12 done

--- Data/Models/12/train.py
+++ Data/Models/12/train.py
@@ -9,41 +9,41 @@ import numpy as np
 with open('../../words', 'rb') as fp:
     words = pickle.load(fp)
 
-with open('../../pos_tags', 'rb') as fp:
-    pos_tags = pickle.load(fp)
+# with open('../../pos_tags', 'rb') as fp:
+#     pos_tags = pickle.load(fp)
 
-# with open('../../chunk_tags', 'rb') as fp:
-#     chunk_tags = pickle.load(fp)
+with open('../../chunk_tags', 'rb') as fp:
+    chunk_tags = pickle.load(fp)
 
 # print(words)
 # print(pos_tags)
 # print(chunk_tags)
 
-N = len(words) + len(pos_tags) + \
-    len(words) + len(pos_tags) + 2
+N = len(words) + len(chunk_tags) + \
+    len(words) + len(chunk_tags) + 2
 
 Y = []
 classes = set([])
 
 words_len = len(words)
-pos_len = len(pos_tags)
-# chunk_len = len(chunk_tags)
+# pos_len = len(pos_tags)
+chunk_len = len(chunk_tags)
 
 row_ind = []
 col_ind = []
 
 word_index = {}
-pos_index = {}
-# chunk_index = {}
+# pos_index = {}
+chunk_index = {}
 
 for i in range(len(words)):
     word_index[words[i]] = i
 
-for i in range(len(pos_tags)):
-    pos_index[pos_tags[i]] = i
+# for i in range(len(pos_tags)):
+#     pos_index[pos_tags[i]] = i
 
-# for i in range(len(chunk_tags)):
-#     chunk_index[chunk_tags[i]] = i
+for i in range(len(chunk_tags)):
+    chunk_index[chunk_tags[i]] = i
 
 lr_index = {}
 lr_index["L"] = 0
@@ -61,33 +61,33 @@ with open(sys.argv[1], "r") as f:
 
             if current[0].strip().split(" ")[0].strip() == "ROOT":
                 col_ind.append(word_index["ROOT"])
-                col_ind.append(words_len + pos_index["ROOT"])
+                col_ind.append(words_len + chunk_index["ROOT"])
                 # col_ind.append(words_len + pos_len + chunk_index["ROOT"])
 
             else:
                 # print(current[0].strip().split(" ")[1])
                 col_ind.append(word_index[(current[0].strip().split(" ")[1])])
                 col_ind.append(
-                    words_len + pos_index[(current[0].strip().split(" ")[2])])
+                    words_len + chunk_index[(current[0].strip().split(" ")[2])])
                 # col_ind.append(words_len + pos_len +
                 #                chunk_index[(current[0].strip().split(" ")[3])])
 
             if current[1].strip().split(" ")[0].strip() == "ROOT":
-                col_ind.append(words_len + pos_len + word_index[("ROOT")])
-                col_ind.append(words_len + pos_len +
-                               words_len + pos_tags.index("ROOT"))
+                col_ind.append(words_len + chunk_len + word_index[("ROOT")])
+                col_ind.append(words_len + chunk_len +
+                               words_len + chunk_tags.index("ROOT"))
                 # col_ind.append(words_len + pos_len + chunk_tags.index("ROOT"))
 
             else:
                 # print(current[1].strip().split(" ")[1])
                 col_ind.append(
-                    words_len + pos_len + word_index[(current[1].strip().split(" ")[1])])
-                col_ind.append(words_len + pos_len + words_len +
-                               pos_index[(current[1].strip().split(" ")[2])])
+                    words_len + chunk_len + word_index[(current[1].strip().split(" ")[1])])
+                col_ind.append(words_len + chunk_len + words_len +
+                               chunk_index[(current[1].strip().split(" ")[2])])
                 # col_ind.append(words_len + pos_len + chunk_len + words_len +
                 #                pos_len + chunk_index[(current[1].strip().split(" ")[3])])
 
-            col_ind.append(2 * (words_len + pos_len) +
+            col_ind.append(2 * (words_len + chunk_len) +
                            lr_index[current[2].strip()])
 
             row_ind.extend(repeat(len(Y), 5))
